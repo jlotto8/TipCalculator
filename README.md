199eaaa80eac9a97a9254e20d8ea1f0ca1fe5a43
jlotto8 <jlotto8@gmail.com>
2022-11-13 18:11:48 -0500

    made changes based on instructor feedback

--- README.md
+++ README.md
@@ -1,30 +1,67 @@
 # TipRepo
 Project 1-Tip Calculator
+This tip calculator app created as the first project assigned for the computer programming intensive Justice Through Code at Columbia University. 
 
 # Description
-This program runs properly in python from the command line, and does not require  any sofftware  or  external libraries. It takes user input to calculate the amount each person owes on a bill, including tax and tip
-
-# How it Works
-Asks for user input for how many people in the group
-Asks for user input for the amount of the bill
-Asks for user input for what percent tip the would like to leave, with options
-Calculates the tip amount
-Calculates the sales tax amount
-Calculates the bill, tip, and sales tax total
-Splits the total by thee amount of people in the group
-
-Repeat tip calculation:
-    After calculating the total bill, and how much each person owes, the user will be asked if they would like to add any additinal tip
-
-Repeat the program:
-    After the repeat tip calculation, the user will be asked if it wants to start again. If so, the program will run again from the beginning
-
-# Usage
-The tip calculator runs with python3
+This program runs properly in python from the command line, and does not require  any sofftware  or  external libraries. It takes user input to calculate the amount each person owes on a bill, including tax and tip.
 
 # From the terminal
 Clone this repo
 CD into the repo folder
 Run python3 tip_calculator.py
 
-![jtc_banner](https://user-images.githubusercontent.com/7483633/131503466-11c7-4444-83eb-fb8bf10a3c78.jpeg)
+# Clone the Tip Calculator Repository
+Copy the code below and paste into your command line:
+https://github.com/jlotto8/TipCalculator.git 
+
+# Run the Tip Calculator
+After it has been cloned, open a folder for the repository, and run the program by entering:
+python3 tip_calculator.py
+
+# Usage
+The tip calculator runs with python3
+Please note that 'python3' is used for the latest version of python that you have installed. Try 'python' or 'py' as alternatives. 
+
+# How it Works/User Prompts
+Once the program is running, the following questions/prompts will appear.
+Asks for user input for the amount of the bill
+Asks for user input for how many people in the group
+Asks for user input for what percent tip the would like to leave, with options
+
+These questions will only accept numbers. If invalid data is used as input, an error message will be displayed with instructions of how to enter valid data. 
+
+The app will perform the following calculations:
+Calculates the tip amount
+Calculates the sales tax amount
+Calculates the bill, tip, and sales tax total
+Splits the total by thee amount of people in the group
+
+# Sample Input and Output 
+
+Inputs
+Food costs $15
+1 person paying
+20% tip
+Expected output:
+
+Total bill: $19.50
+
+
+Inputs
+Food costs $25000000
+3 people paying
+31% tip
+Expected output:
+
+Total bill: $35,250,000.00
+Each person should pay $11,750,000.00
+
+
+# Repeat tip calculation:
+    After calculating the total bill, and how much each person owes, the user will be asked if they would like to add any additinal tip
+
+# Repeat the program:
+    After the repeat tip calculation, the user will be asked if it wants to start again. If so, the program will run again from the beginning
+
+
+
--- tip_calculator.py
+++ tip_calculator.py
@@ -1,20 +1,21 @@
-# create a function that will perform and display the calculations for a tip
+# create a function that taks in input
 def bill_with_tip():
     print('This is a tip calculator') # introduce the app
-    # create global variables that will be accessible to each function
-    persons = -1
-    bill = -2
-    tip = -3
-# create a variable and assign integer input to it for how many people in the group
+
     try: # use a try/except block to account for a value error, if the user enters a string instead of a float or integer-this will prevent the code from breaking
-        persons = int(input('How many people are in your group?'))
-# create a variable and assign float input to it for the bill amount
-        bill = float(input('What is the total amount of the bill? $'))
-# create a variable and assign float input to it for the tip
+
+        # create a variable and assign float input to it for the bill amount
+        bill = float(input('What is the total amount of the bill? $ '))
+        # create a variable  and assign integer input to itfor how many people will be splitting the bill
+        persons = int(input('How many people are in your group? '))
+        # create a variable and assign float input to it for the tip
         tip = float(input('What percentage of tip do you want to leave? 0, 5, 10, 15, 20, 25 ? '))
+         # return tip_calculator(persons, bill, tip) # calls a function within a function- so that the data is avaiable for each function throughoout the entire program
+        return tip_calculator(persons, bill, tip)
     except ValueError:
         print('ValueError - please enter numbers')
-    return tip_calculator(persons, bill, tip) # calls a function within a function- so that the data is avaiable for each function throughoout the entire program *recursion*
+        return bill_with_tip()
+   
 
 # create a function to return the tip and tax
 def tip_calculator(persons, bill, tip):
@@ -22,7 +23,8 @@ def tip_calculator(persons, bill, tip):
     tax = float(bill * 0.10)
     total_bill = (bill + tip_percent + tax)
     round_bill = round(total_bill, 2)
-    final_total = str(round_bill)
+    final_total = (round_bill)
+    print(type(final_total))
     bill_by_person = (round((total_bill)/persons, 2))
     print(f'The total bill including tax and tip is $ {round_bill}.')
     print(f'The amount due per person is $ {bill_by_person}.')
@@ -31,10 +33,11 @@ def tip_calculator(persons, bill, tip):
 # create a function to ask the user if they would like to leave any additional tip, if they choose yes, it will add that to the final bill, and display it
 # if user does not enter 'Y' or 'N' the block will simply run again, to ask the user, and continue until a valid input (Y, N) is entered
 def more_tip(final_total):
-    extra_tip = input('Would you like to enter another tip? Y or N').strip() # .strip() allows the user to enter a space before answering the question, without confusing the program
+    extra_tip = input('Would you like to enter another tip? Y or N ').upper()
     if extra_tip == 'Y': 
-        extra_choice = input('How much more would you like to leave?')
+        extra_choice = float(input('How much more would you like to leave?  '))
         bonus_total = final_total + extra_choice
+        # return bonus_total
         print(f'Your total is {bonus_total}')
         return run_again()
     elif extra_tip == 'N': # if user does not want to add any additional tip, the final will display, and the loop will end
@@ -42,14 +45,14 @@ def more_tip(final_total):
         print(f'Your total is {final_total}')
         return run_again()
     else:
-        print('Please enter Y or N')
+        print('Please enter Y or N ')
         return more_tip(final_total) # calls a function within a function- so that the data is avaiable for each function throughoout the entire program *recursion*
 
 # create a function that runs the entire prgram again if the user chooses to do so
 def run_again():
     finished = True
     while finished is True:
-        finished = input('Would you like to start over? Y or N').strip()
+        finished = input('Would you like to start over? Y or N ').upper()
         if finished == 'Y':
             bill_with_tip()
         else:
